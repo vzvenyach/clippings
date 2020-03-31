2f3455d4798ffde57be798338063b77f81e6bacb
V David Zvenyach <dave@esq.io>
2020-03-30 20:46:49 -0500

    Check for pandoc dependency

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const articles = require("./lib/articles.js");
 const articles2ePub = require("./lib/write.js");
 const { program } = require("commander");
+const config = require("./lib/config.js");
 
 program.version("0.2.0");
 
@@ -10,6 +11,7 @@ program
   .command("config")
   .description("Configure this thing...")
   .action(function() {
+    config.check_pandoc();
     console.log("Not implemented...");
   });
 
@@ -17,6 +19,7 @@ program
   .command("run <outfile> <urls...>")
   .description("Run this thing...")
   .action(function(outfile, urls) {
+    config.check_pandoc(); // Check even if config check fails
     urls.slice(3, process.argv.length + 1);
     articles.getArticlesFromUrlArray(urls).then(a => {
       articles2ePub(a, outfile);
